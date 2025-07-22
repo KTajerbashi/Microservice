d2a20eda1f48e3e0daf908cee0e17a710d897e97
KTajerbashi <kamrantajerbashi@gmail.com>
2025-07-22 13:52:06 +0330

    Feature Dashboard Ready With Blazor

--- ApiDashboard/Models/LogModel.cs
+++ ApiDashboard/Models/LogModel.cs
@@ -1,17 +1,9 @@
 ﻿namespace ApiDashboard.Models;
 
-// Models/UserModel.cs
-public class UserModel
-{
-    public long Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Family { get; set; } = string.Empty;
-    public string Email => $"{Name}_{Family}@mail.com";
-}
-
 // Models/LogModel.cs
 public class LogModel
 {
+    public long Id { get; set; }
     public bool IsCompleted { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
---
+++ ApiDashboard/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiDashboard.Models;
+
+// Models/UserModel.cs
+public class UserModel
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Family { get; set; } = string.Empty;
+    public string Email => $"{Name}_{Family}@mail.com";
+}
+
+
+public class CallAction
+{
+    public string ActionName { get; set; }
+    public bool Repeatly { get; set; }
+    public int RepeatlyCount { get; set; }
+}
