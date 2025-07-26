5213de35d636f880487bbcd39fefa07c8c919596
KTajerbashi <kamrantajerbashi@gmail.com>
2025-07-26 22:53:02 +0330

    #Feature Add Api Gateway Changes and Published

---
+++ Src/WebApi/ApiGateway/Architectures.ApiGateway/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Architectures.ApiGateway.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok("Success ...");
+    }
+}
--- Src/WebApi/ApiGateway/Architectures.ApiGateway/Hosting/ApiGateway/YarpApp.cs
+++ Src/WebApi/ApiGateway/Architectures.ApiGateway/Hosting/ApiGateway/YarpApp.cs
@@ -1,12 +1,32 @@
-﻿namespace Architectures.ApiGateway.Hosting.ApiGateway;
+﻿using Ocelot.Values;
+using Yarp.ReverseProxy.Transforms;
+
+namespace Architectures.ApiGateway.Hosting.ApiGateway;
 
 public static class YarpApp
 {
     public static WebApplication StartYarp(this WebApplicationBuilder builder)
     {
         // Add YARP services
-        builder.Services.AddReverseProxy()
-            .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+        //builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+        builder.Services
+           .AddReverseProxy()
+           .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+           .AddTransforms(builderContext =>
+           {
+               builderContext.AddRequestTransform(async context =>
+               {
+                   var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+                   logger.LogInformation($"Request from {context.HttpContext.Connection.RemoteIpAddress} to {context.HttpContext.Request.Path}");
+               });
+               builderContext.AddResponseTransform(async context =>
+               {
+                   var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+                   logger.LogInformation($"Response for {context.HttpContext.Request.Path} - Status: {context.HttpContext.Response.StatusCode}");
+               });
+           });
+
 
         return builder.Build();
     }
